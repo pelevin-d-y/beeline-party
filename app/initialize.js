fe083bfe3b9a2cf95f0c7b69d119281d211c118f
pelevin-d-y <natpuot1992@gmail.com>
2019-01-15 14:56:15 +0300

    comment seating

--- app/initialize.js
+++ app/initialize.js
@@ -27,7 +27,7 @@ jQuery('.location__map-link').fancybox({
 
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors:['page-1','page-2', 'page-3', 'page-4', 'page-5'],
+    anchors:['page-1','page-2', 'page-3', 'page-4'],
     menu: '#menu',
     navigation: true
   });
